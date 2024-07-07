871ebed53b20c7384c920f1dce2ee53096afde44
Ayush Gaurav <163287937+ayushgaurav04@users.noreply.github.com>
2024-07-07 19:41:01 +0530

    first

---
+++ script.js
@@ -0,0 +1,78 @@
+gsap.from("nav",{
+opacity:0,
+duration:3,
+delay: 1
+
+
+
+})
+gsap.from(".log1 img",{
+    opacity:0,
+    duration:2,
+    delay: 1,
+    rotate: "20",
+    scale:0,
+    
+    
+
+
+})
+gsap.from(".text_container_1",{
+    opacity:0,
+    duration:1,
+    delay: 1
+    
+
+
+})
+gsap.from("#box1",{
+    opacity:0,
+    duration:2,
+    delay: 1.5
+    
+
+
+})
+gsap.from(".text_container_1 h1",{
+    opacity:0,
+    duration:1,
+    delay: 1,
+    x:-10
+    
+
+
+})
+gsap.from(".text_container_1 h2",{
+    opacity:0,
+    duration:1,
+    delay: 2,
+    x:-10
+    
+
+
+})
+gsap.from(".text_container_1 p",{
+    opacity:0,
+    duration:2,
+    delay: 2,
+    x:-10
+    
+
+
+})
+gsap.from("#box2",{
+    opacity:0,
+    duration:2,
+    delay: 2
+    
+
+
+})
+gsap.from("#box3",{
+    opacity:0,
+    duration:4,
+    delay: 2.2
+    
+
+
+})
